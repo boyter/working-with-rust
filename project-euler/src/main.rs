c2ec40e77141d4b4e716761a9c45e6e090f7d149
Ben Boyter <ben@boyter.org>
2017-09-14 17:55:12 +1000

    Remove redundant variable

--- project-euler/src/main.rs
+++ project-euler/src/main.rs
@@ -5,19 +5,9 @@ mod euler7;
 mod euler35;
 
 fn main() {
-    let answer_1 = euler1::euler_1();
-    println!("{}", answer_1);
-
-    let answer_2 = euler2::euler_2();
-    println!("{}", answer_2);
-
-    let answer_3 = euler3::euler_3();
-    println!("{}", answer_3);
-
-    let answer_7 = euler7::euler_7();
-    println!("{}", answer_7);
-
-    let answer_35 = euler35::euler_35();
-    println!("{}", answer_35);
+    println!("{}", euler1::euler_1());
+    println!("{}", euler2::euler_2());
+    println!("{}", euler3::euler_3());
+    println!("{}", euler7::euler_7());
+    println!("{}", euler35::euler_35());
 }
-
